ba7167035b808ddd0e37478de4fc4d3b158b9fc9
fanhaotian <1592276395@qq.com>
2024-12-02 23:46:47 +0800

    update utils.

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,9 @@
 """
 
 import tiktoken
-
 import config
+import pymysql
+from config import DATABASE_CONFIG
 
 enc = tiktoken.encoding_for_model(config.MODEL)
 
@@ -12,3 +13,40 @@ def count_token(text):
     """计算text对应的token数"""
     tokens = enc.encode(text)
     return len(tokens)
+
+
+def get_db_connection():
+    """建立数据库连接"""
+    conn = pymysql.connect(
+        host=DATABASE_CONFIG["host"],
+        user=DATABASE_CONFIG["user"],
+        password=DATABASE_CONFIG["password"],
+        port=DATABASE_CONFIG["port"],
+        charset=DATABASE_CONFIG["charset"],
+        database=DATABASE_CONFIG["database"],
+    )
+    return conn
+
+
+def query_database(conn, sql):
+    """执行查询语句并返回结果"""
+    cursor = conn.cursor()
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    finally:
+        cursor.close()
+        # conn.close()
+    return results
+
+
+def sql_in_text(text):
+    """判断text中是否有sql语句"""
+    return config.SQL_START in text and config.SQL_END in text
+
+
+def extract_sql(text):
+    """从text中提取sql语句"""
+    start = text.find(config.SQL_START) + len(config.SQL_START)
+    end = text.find(config.SQL_END)
+    return text[start:end].strip()
